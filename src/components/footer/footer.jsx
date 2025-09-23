3a3f0e85728f62e665923a5d680da5b88435f68c
renzo <renzogpereira@gmail.com>
2025-09-23 13:25:54 -0300

    Version 0.9

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -1,34 +1,7 @@
-import { useRef } from "react";
-import { motion } from "framer-motion";
-import emailjs from "emailjs-com";
 import logo from "../../assets/Logo Renzo Developer.png";
+import { motion } from "framer-motion";
 
 const Footer = () => {
-  const form = useRef();
-
-  const sendEmail = (e) => {
-    e.preventDefault();
-
-    emailjs
-      .sendForm(
-        "service_aqsiu1r",  // reemplaza con tu Service ID de EmailJS
-        "template_20iwz3e", // reemplaza con tu Template ID
-        form.current,
-        "-ee6RkGGLv3eUDiZf"   // reemplaza con tu Public Key
-      )
-      .then(
-        (result) => {
-          console.log(result.text);
-          alert("Mensaje enviado correctamente!");
-          e.target.reset(); // limpia el formulario
-        },
-        (error) => {
-          console.log(error.text);
-          alert("Error al enviar el mensaje");
-        }
-      );
-  };
-
   return (
     <footer id="contacto" className="container-fluid mt-5 py-5">
       <div className="container d-flex justify-content-around flex-column flex-md-row">
@@ -36,24 +9,16 @@ const Footer = () => {
           <h2>Contacto</h2>
           <ul className="list-unstyled">
             <li>
-              <a className="a-footer" href="mailto:renzoGpereira@gmail.com" rel="noopener noreferrer">
-                <i className="icono-footer bi bi-envelope-at-fill"></i> renzoGpereira@gmail.com
-              </a>
+              <a href="mailto:renzoGpereira@gmail.com" rel="noopener noreferrer"><i className="bi bi-envelope-at-fill"></i> renzoGpereira@gmail.com</a>
             </li>
             <li>
-              <a className="a-footer" href="https://maps.app.goo.gl/wntxzAQ7i1e22aGc7" target="_blank" rel="noopener noreferrer">
-                <i className="icono-footer bi bi-geo-alt-fill"></i> Canelones, Uruguay
-              </a>
+              <a href="https://maps.app.goo.gl/wntxzAQ7i1e22aGc7" target="_blank" rel="noopener noreferrer"><i className="bi bi-geo-alt-fill"></i> Canelones, Uruguay</a>
             </li>
             <li>
-              <a className="a-footer" href="https://github.com/Renzo-Pereira" target="_blank" rel="noopener noreferrer">
-                <i className="icono-footer bi bi-github"></i> Renzo Pereira
-              </a>
+              <a href="https://github.com/Renzo-Pereira" target="_blank" rel="noopener noreferrer"><i className="bi bi-github"></i> Renzo Pereira</a>
             </li>
             <li>
-              <a className="a-footer" href="https://www.linkedin.com/in/renzo-pereira-832b7724b/" target="_blank" rel="noopener noreferrer">
-                <i className="icono-footer bi bi-linkedin"></i> Renzo Pereira
-              </a>
+              <a href="https://www.linkedin.com/in/renzo-pereira-832b7724b/" target="_blank" rel="noopener noreferrer"><i className="bi bi-linkedin"></i> Renzo Pereira</a>
             </li>
           </ul>
           <motion.img
@@ -65,19 +30,23 @@ const Footer = () => {
             viewport={{ once: true, amount: 0.5 }}
             transition={{ duration: 2, ease: "easeOut" }}
           />
-        </div>
 
-        <form ref={form} onSubmit={sendEmail} className="d-flex flex-column gap-2">
+        </div>
+        <form className="d-flex flex-column gap-2">
           <label className="form-label">Nombre</label>
-          <input placeholder="Nombre" name="nombre" type="text" className="form-control" required />
-          
+          <input placeholder="Nombre" type="text" className="form-control" />
           <label className="form-label">Email</label>
-          <input placeholder="Email" name="email" type="email" className="form-control" required />
-          
+          <input placeholder="Email" type="email" className="form-control" />
           <label className="form-label">Mensaje</label>
-          <textarea placeholder="Mensaje" name="mensaje" className="form-control" cols="30" rows="10" required></textarea>
-          
-          <button type="submit" className="btn btn-primary m-0 mt-2">Enviar</button>
+          <textarea
+            className="form-control"
+            placeholder="Mensaje"
+            name=""
+            id=""
+            cols="30"
+            rows="10"
+          ></textarea>
+          <input type="submit" className="btn-primary m-0 mt-2" />
         </form>
       </div>
     </footer>
